6c3dbe640527820879c43d8dc9899ab9ab73c79b
Rumply <rumplygnat@hotmail.com>
2018-05-27 22:54:40 -0400

    Update PoppyHoleCustomEvent.js

--- PoppyHoleCustomEvent.js
+++ PoppyHoleCustomEvent.js
@@ -15,13 +15,15 @@ var nameDictionnary = {
 	'5b05443afc0328001eac983e':'jeremiah','5b0150096bc650af735accc5':'Rosie ?',
 	'5b0b1872ff7fce0032adea03':'Max','5b0622bd87c62f001e192452':'Rumply',
 	'5b01f89c7d60b6001e835793':'yrace ?','5b0a8a27b8d768003c5595f1':'Collin',
-    	'5b09e1a5b56f22001e760744':'Oliver','5b0150496bc650af735acd57':'Nelly',
+	'5b09e1a5b56f22001e760744':'Oliver','5b0150496bc650af735acd57':'Nelly',
 	'5b05fca83802b5004606bb0a':'damien','5b063a54faf7ed001e273a26':'Bailey',
 	'5b00a704cf5dc883d3d8db44':'lum','5b05ed3b3802b5004606b972':'EternalKnight',
 	'5b07e725f9bc8600325d051e':'Pixie Queen','5b05b61f62347400284d375b':'Jack',
 	'5b034a2927a3c6001e0d2dc8':'Lightbug01 (ellen)','5b049b849180f4001e5c38bb':'ZEE_DAWG32',
 	'5b07adcbbad68b001e27c48c':'FauxUnknown','5b05c06346745e003292a559':'Evelyn',
-	'5b01f87a7d60b6001e835719':'ALEX'
+	'5b01f87a7d60b6001e835719':'ALEX','5b080dca0e7ace00465c9d25':'Josh',
+	'5b02a14a7d60b6001e835beb':'Lazarus','5b0aa1d6b8d768003c559776':'Matt Lo',
+	'5b05ece83802b5004606b857':'Jun Le'
 };
 
 // Functions
